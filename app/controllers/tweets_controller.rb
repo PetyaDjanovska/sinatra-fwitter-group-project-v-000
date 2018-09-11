7b3e231dc60f50e716522e0ce9bc01256d0e706b
Petya Djanovska <petya.djanovska@gmail.com>
2018-09-11 15:31:07 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,8 +5,10 @@ class TweetsController < ApplicationController
   end
 
   post '/tweets' do
-    @user = current_user
-
+    if logged_in?
+      @tweet = Tweet.new(params[:tweet])
+      @tweet.user_id = current_user.id
+    end
   end
 
 end
