f0c510bf1fe3263c68c490393b9f739d92241809
Petya Djanovska <petya.djanovska@gmail.com>
2018-09-11 15:13:42 +0000

    automatically backed up by learn

--- db/migrate/20180911151322_create_tweets.rb
+++ db/migrate/20180911151322_create_tweets.rb
@@ -1,4 +1,8 @@
 class CreateTweets < ActiveRecord::Migration
   def change
+    create_table :tweets do |t|
+      t.string :username
+      t.string :email
+      t.string :password_digest
   end
 end
