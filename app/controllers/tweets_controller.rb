6a4cf9291e485f8fd796a97b119035d63eb5f8ae
Petya Djanovska <petya.djanovska@gmail.com>
2018-09-11 15:50:24 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -33,7 +33,7 @@ class TweetsController < ApplicationController
   post '/tweets/:id' do
     @tweet = Tweet.find(params[:id])
    if logged_in? && @tweet.user_id == current_user.id
-     @tweet.update(content: => params[:tweet]["content"])
+     @tweet.update(content: => params([:tweet]["content"])
      @campsite.save
      redirect '/users/home'
    else
